bb2bbd2670129dc44ab910aef5c8e0abdc212aee
Ryûga <corentingambier@gmail.com>
2023-09-11 19:43:48 +0200

    DM1

--- main.c
+++ main.c
@@ -1,44 +1,68 @@
 #include <stdio.h>
 #include "fonctions.c"
-int pplnumber;
-int tablenum;
-int drink;
-int result_drink;
-double price = 0;
-int eat;
 
 int main() {
-    printf("How many are you ?\n");
-    scanf("%d",&pplnumber);
+    int pplnumber;
+    int tablenum;
+    int drink;
+    char result_drink;
+    double price = 0;
+    char eat;
+    char dessert;
+
+    printf("How many are you?\n");
+    scanf("%d", &pplnumber);
     tablenum = tablenumber(pplnumber);
-    printf("Your table is the number: %d\n",tablenum);
-    printf("Now that you're installed do you want to drink something ? enter 1 for yes or 2 for no\n");
-    scanf("%d",&drink);
-    if(drink == 1 ){
-        printf("enter s for a soda, w for water or l for lemonade:\n ");
-        scanf("%d",result_drink);
-        if(result_drink == 's'){
-            price = price + 5;
-        }else if (result_drink == 'w'){
-            price = price + 2;
-        }else if (result_drink == 'l'){
-            price = price + 12;
-        }else printf("an error occured\n");
+    printf("Your table is the number: %d\n", tablenum);
+
+    printf("Now that you're seated, do you want to drink something? Enter 1 for yes or 2 for no\n");
+    scanf("%d", &drink);
+
+    if (drink == 1) {
+        printf("Enter 's' for a soda, 'w' for water, or 'l' for lemonade:\n ");
+        scanf(" %c", &result_drink);
+
+        if (result_drink == 's') {
+            price += 5;
+        } else if (result_drink == 'w') {
+            price += 2;
+        } else if (result_drink == 'l') {
+            price += 12;
+        } else {
+            printf("An error occurred\n");
+        }
+    } else {
+        printf("So let's move on to the main course!\n");
     }
-   else printf("so let's move on the main course !\n");
 
-   printf( "Ok,so you have the choice between entering : c for a couscous, t for Tomatoes or R for a risotto ? Which one do you want to eat ?\n");
-   scanf("%d",&eat);
-   if(eat== 'c'){
-       price = price +15;
-   }else if(eat == 't' ){
-       price = price + 8;
-   }else if( eat == 'r'){
-       price = price + 23;
-   }else printf("I guess there is nothing that is to your taste...\n ");
+    printf("OK, so you have the choice between entering 'c' for a couscous, 't' for tomatoes, or 'r' for a risotto. What do you want to eat?\n");
+    scanf(" %c", &eat);
 
+    if (eat == 'c') {
+        price += 15;
+    } else if (eat == 't') {
+        price += 8;
+    } else if (eat == 'r') {
+        price += 23;
+    } else {
+        printf("I guess there is nothing that suits your taste...\n");
+    }
 
+    printf("So to end your meal, what would you like for dessert? You have the choice between 't' for tiramisu, 'g' for ice cream, or 'm' for macarons!\n");
+    scanf(" %c", &dessert);
 
+    if (dessert == 't') {
+        price += 62;
+    } else if (dessert == 'g') {
+        price += 3;
+    } else if (dessert == 'm') {
+        price += 8;
+    } else {
+        printf("I understand it seems like you've already eaten a lot...\n");
+    }
+    price *= pplnumber;
+
+    printf("Total price: %.2lf$\n", price);
 
     return 0;
 }
